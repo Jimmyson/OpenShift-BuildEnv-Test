1307f0c031a127e0df7e137be340e23aee044b9d
Jimmyson <Jimmyson@users.noreply.github.com>
2022-07-01 10:30:04 +1000

    Update build.sh
    Remove command invoke in Echo

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ chmod a+x ./kickstart.sh
 echo "DEV = $DEV"
 
 if [ $DEV ]; then
-    echo "I hit Dev Mode... Running command `./kickstart.sh -d`"
+    echo "I hit Dev Mode... Running command './kickstart.sh -d'"
     ./kickstart.sh -d
 else
-    echo "I did not hit Dev Mode... Running command `./kickstart.sh`"
+    echo "I did not hit Dev Mode... Running command './kickstart.sh'"
     ./kickstart.sh
 fi
