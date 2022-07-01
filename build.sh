34d6e04f263416a79f29be3b82a29cd5dfe224ce
Jimmyson <Jimmyson@users.noreply.github.com>
2022-07-01 10:28:22 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 set -e
 
 chmod a+x ./kickstart.sh
+echo "DEV = $DEV"
+
 if [ $DEV ]; then
+    echo "I hit Dev Mode... Running command `./kickstart.sh -d`"
     ./kickstart.sh -d
 else
+    echo "I did not hit Dev Mode... Running command `./kickstart.sh`"
     ./kickstart.sh
 fi
